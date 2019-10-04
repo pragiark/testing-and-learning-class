9f9fc1255b32e8adf0f67c6f569e13cd7b8a2146
pragiark <pragiark@wp.pl>
2019-10-04 23:26:24 +0200

    Add method update odometer

--- Car.py
+++ Car.py
@@ -5,7 +5,7 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
-        self.odometer_reading = 0
+        self.odometer_reading = 800
 
     def get_descriptive_name(self):
         """Print car"""
@@ -16,8 +16,16 @@ class Car():
         """Add odometer method"""
         print("Ten samochód ma przejechane " + str(self.odometer_reading) + " km.")
 
+    def update_odometer(self, mileage):
+        """Update odometer in method"""
+        if mileage < self.odometer_reading:
+            print("Nie mozna cofać licznika")
+        else:
+            self.odometer_reading = mileage
+
+
 
 my_new_car = Car("ford", "focus", 2008)
 print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 77
-my_new_car.read_odometer()
+my_new_car.update_odometer(777)
+#my_new_car.read_odometer()
