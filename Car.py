e1f7aebd7350b08cfa51e6515b3117496bd38368
pragiark <pragiark@wp.pl>
2019-10-10 00:37:41 +0200

    Adding battery class and fill gast tank

--- Car.py
+++ Car.py
@@ -5,12 +5,13 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
-        self.odometer_reading = 800
+        self.odometer_reading = 0
+
 
     def get_descriptive_name(self):
         """Print car"""
         long_name = str(self.year) + " " + self.make + " " + self.model
-        return long_name.title()
+        print(long_name.title())
 
     def read_odometer(self):
         """Add odometer method"""
@@ -27,10 +28,54 @@ class Car():
         """Increment odometer method"""
         self.odometer_reading += kilometer
 
+    def fill_gas_tank(self, gas_tank):
+        """Add gas tank"""
+        self.gas_tank = gas_tank
+
+    def read_gas_tank(self):
+        """"Print gas tank"""
+        print(self.make + ", " + self.model + " ma zbiornik paliwa o pojemności " + str(self.gas_tank) + " litrów")
+
+class ElectricCar(Car):
+    """Electric car class"""
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def fill_gas_tank(self, gas_tank):
+        """No gas_tank information"""
+        print("Nie dodam tego, elektryczny samochód nie ma zbiornika paliwa")
+
+    def read_gas_tank(self):
+        """No gas_tank information"""
+        print("Elektryczny samochód nie posiada zbiornika paliwa")
+
+class Battery():
+    """Class battery to electric car"""
+    def __init__(self, battery_size = 70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """"Print battery size information"""
+        print("Ten samochód ma akumulator o pojemności " + str(self.battery_size) + " kWh")
 
 
-my_new_car = Car("ford", "focus", 2008)
-print(my_new_car.get_descriptive_name())
+
+focus = Car("ford", "focus", 2008)
+focus.get_descriptive_name()
 #my_new_car.update_odometer(777)
-my_new_car.increment_odometer(77)
-my_new_car.read_odometer()
+focus.increment_odometer(77)
+focus.read_odometer()
+focus.fill_gas_tank(45)
+focus.read_gas_tank()
+
+tesla = ElectricCar("Tesla", "Model S", 2018)
+tesla.get_descriptive_name()
+tesla.read_odometer()
+tesla.update_odometer(0)
+tesla.read_odometer()
+tesla.fill_gas_tank(20)
+tesla.read_gas_tank()
+
+tesla.battery.describe_battery()
+
